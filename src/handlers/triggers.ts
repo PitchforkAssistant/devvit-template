bed9d568989cc1e9dfc5ac725c4d048b9f82003f
PitchforkAssistant <pitchforkassistant@gmail.com>
2023-09-06 09:35:37 +0300

    Context -> TriggerContext

--- src/handlers/triggers.ts
+++ src/handlers/triggers.ts
@@ -3,75 +3,75 @@ import {
     PostSubmit, PostCreate, PostUpdate, PostDelete, PostReport, PostFlairUpdate,
     CommentSubmit, CommentCreate, CommentUpdate, CommentDelete, CommentReport,
 } from "@devvit/protos";
-import {Context, OnTriggerEvent, TriggerEventType, TriggerEvent} from "@devvit/public-api";
+import {TriggerContext, OnTriggerEvent, TriggerEventType, TriggerEvent} from "@devvit/public-api";
 import {logError} from "../helpers/miscHelpers.js";
 
 // Post Triggers
 
-export async function onPostSubmit (event: OnTriggerEvent<PostSubmit>, context: Context) {
+export async function onPostSubmit (event: OnTriggerEvent<PostSubmit>, context: TriggerContext) {
     return onTrigger(event, context);
 }
 
-export async function onPostCreate (event: OnTriggerEvent<PostCreate>, context: Context) {
+export async function onPostCreate (event: OnTriggerEvent<PostCreate>, context: TriggerContext) {
     return onTrigger(event, context);
 }
 
-export async function onPostUpdate (event: OnTriggerEvent<PostUpdate>, context: Context) {
+export async function onPostUpdate (event: OnTriggerEvent<PostUpdate>, context: TriggerContext) {
     return onTrigger(event, context);
 }
 
-export async function onPostDelete (event: OnTriggerEvent<PostDelete>, context: Context) {
+export async function onPostDelete (event: OnTriggerEvent<PostDelete>, context: TriggerContext) {
     return onTrigger(event, context);
 }
 
-export async function onPostReport (event: OnTriggerEvent<PostReport>, context: Context) {
+export async function onPostReport (event: OnTriggerEvent<PostReport>, context: TriggerContext) {
     return onTrigger(event, context);
 }
 
-export async function onPostFlairUpdate (event: OnTriggerEvent<PostFlairUpdate>, context: Context) {
+export async function onPostFlairUpdate (event: OnTriggerEvent<PostFlairUpdate>, context: TriggerContext) {
     return onTrigger(event, context);
 }
 
 // Comment Triggers
 
-export async function onCommentSubmit (event: OnTriggerEvent<CommentSubmit>, context: Context) {
+export async function onCommentSubmit (event: OnTriggerEvent<CommentSubmit>, context: TriggerContext) {
     return onTrigger(event, context);
 }
 
-export async function onCommentCreate (event: OnTriggerEvent<CommentCreate>, context: Context) {
+export async function onCommentCreate (event: OnTriggerEvent<CommentCreate>, context: TriggerContext) {
     return onTrigger(event, context);
 }
 
-export async function onCommentUpdate (event: OnTriggerEvent<CommentUpdate>, context: Context) {
+export async function onCommentUpdate (event: OnTriggerEvent<CommentUpdate>, context: TriggerContext) {
     return onTrigger(event, context);
 }
 
-export async function onCommentDelete (event: OnTriggerEvent<CommentDelete>, context: Context) {
+export async function onCommentDelete (event: OnTriggerEvent<CommentDelete>, context: TriggerContext) {
     return onTrigger(event, context);
 }
 
-export async function onCommentReport (event: OnTriggerEvent<CommentReport>, context: Context) {
+export async function onCommentReport (event: OnTriggerEvent<CommentReport>, context: TriggerContext) {
     return onTrigger(event, context);
 }
 
 // Mod Triggers
-export async function onModMail (event: OnTriggerEvent<ModMail>, context: Context) {
+export async function onModMail (event: OnTriggerEvent<ModMail>, context: TriggerContext) {
     return onTrigger(event, context);
 }
 
-export async function onModAction (event: OnTriggerEvent<ModAction>, context: Context) {
+export async function onModAction (event: OnTriggerEvent<ModAction>, context: TriggerContext) {
     return onTrigger(event, context);
 }
 
 // All triggers above are there as examples, they all call this function that takes any trigger event.
 
-export async function onTrigger (event: OnTriggerEvent<TriggerEventType[TriggerEvent]>, context: Context) {
+export async function onTrigger (event: OnTriggerEvent<TriggerEventType[TriggerEvent]>, context: TriggerContext) {
     console.log(`type: ${event.type}\nevent:\n${JSON.stringify(event)}\ncontext:\n${JSON.stringify(context)}`);
 }
 
 // App Meta Triggers
 
-export async function onAppChanged (event: OnTriggerEvent<AppInstall | AppUpgrade>, context: Context) {
+export async function onAppChanged (event: OnTriggerEvent<AppInstall | AppUpgrade>, context: TriggerContext) {
     console.log(`onAppChanged\nevent:\n${JSON.stringify(event)}\ncontext:\n${JSON.stringify(context)}`);
     try {
         // Cancel any existing jobs.
