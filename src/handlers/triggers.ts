8b1827e2afd9405d7d7446d37ade9d1aed9e2bad
PitchforkAssistant <pitchforkassistant@gmail.com>
2024-02-25 05:46:16 +0200

    bump dependencies

--- src/handlers/triggers.ts
+++ src/handlers/triggers.ts
@@ -3,63 +3,63 @@ import {
     PostSubmit, PostCreate, PostUpdate, PostDelete, PostReport, PostFlairUpdate,
     CommentSubmit, CommentCreate, CommentUpdate, CommentDelete, CommentReport,
 } from "@devvit/protos";
-import {TriggerContext, OnTriggerEvent} from "@devvit/public-api";
+import {TriggerContext} from "@devvit/public-api";
 import {onAnyTriggerConsoleLog, startSingletonJob} from "devvit-helpers";
 
 // Post Triggers
 
-export async function onPostSubmit (event: OnTriggerEvent<PostSubmit>, context: TriggerContext) {
+export async function onPostSubmit (event: PostSubmit, context: TriggerContext) {
     return onAnyTriggerConsoleLog(event, context);
 }
 
-export async function onPostCreate (event: OnTriggerEvent<PostCreate>, context: TriggerContext) {
+export async function onPostCreate (event: PostCreate, context: TriggerContext) {
     return onAnyTriggerConsoleLog(event, context);
 }
 
-export async function onPostUpdate (event: OnTriggerEvent<PostUpdate>, context: TriggerContext) {
+export async function onPostUpdate (event: PostUpdate, context: TriggerContext) {
     return onAnyTriggerConsoleLog(event, context);
 }
 
-export async function onPostDelete (event: OnTriggerEvent<PostDelete>, context: TriggerContext) {
+export async function onPostDelete (event: PostDelete, context: TriggerContext) {
     return onAnyTriggerConsoleLog(event, context);
 }
 
-export async function onPostReport (event: OnTriggerEvent<PostReport>, context: TriggerContext) {
+export async function onPostReport (event: PostReport, context: TriggerContext) {
     return onAnyTriggerConsoleLog(event, context);
 }
 
-export async function onPostFlairUpdate (event: OnTriggerEvent<PostFlairUpdate>, context: TriggerContext) {
+export async function onPostFlairUpdate (event: PostFlairUpdate, context: TriggerContext) {
     return onAnyTriggerConsoleLog(event, context);
 }
 
 // Comment Triggers
 
-export async function onCommentSubmit (event: OnTriggerEvent<CommentSubmit>, context: TriggerContext) {
+export async function onCommentSubmit (event: CommentSubmit, context: TriggerContext) {
     return onAnyTriggerConsoleLog(event, context);
 }
 
-export async function onCommentCreate (event: OnTriggerEvent<CommentCreate>, context: TriggerContext) {
+export async function onCommentCreate (event: CommentCreate, context: TriggerContext) {
     return onAnyTriggerConsoleLog(event, context);
 }
 
-export async function onCommentUpdate (event: OnTriggerEvent<CommentUpdate>, context: TriggerContext) {
+export async function onCommentUpdate (event: CommentUpdate, context: TriggerContext) {
     return onAnyTriggerConsoleLog(event, context);
 }
 
-export async function onCommentDelete (event: OnTriggerEvent<CommentDelete>, context: TriggerContext) {
+export async function onCommentDelete (event: CommentDelete, context: TriggerContext) {
     return onAnyTriggerConsoleLog(event, context);
 }
 
-export async function onCommentReport (event: OnTriggerEvent<CommentReport>, context: TriggerContext) {
+export async function onCommentReport (event: CommentReport, context: TriggerContext) {
     return onAnyTriggerConsoleLog(event, context);
 }
 
 // Mod Triggers
-export async function onModMail (event: OnTriggerEvent<ModMail>, context: TriggerContext) {
+export async function onModMail (event: ModMail, context: TriggerContext) {
     return onAnyTriggerConsoleLog(event, context);
 }
 
-export async function onModAction (event: OnTriggerEvent<ModAction>, context: TriggerContext) {
+export async function onModAction (event: ModAction, context: TriggerContext) {
     return onAnyTriggerConsoleLog(event, context);
 }
 
@@ -71,7 +71,7 @@ export async function onModAction (event: OnTriggerEvent<ModAction>, context: Tr
 
 // App Meta Triggers
 
-export async function onAppChanged (event: OnTriggerEvent<AppInstall | AppUpgrade>, context: TriggerContext) {
+export async function onAppChanged (event: AppInstall | AppUpgrade, context: TriggerContext) {
     console.log(`onAppChanged\nevent:\n${JSON.stringify(event)}\ncontext:\n${JSON.stringify(context)}`);
     try {
         // This function from devvit-helpers will start a job, but it terminates any other jobs with the same name first.
