241395ca7c32e450c1c4370c2ae9db7648a39e43
PitchforkAssistant <pitchforkassistant@gmail.com>
2024-06-14 20:30:56 +0300

    change job handler from Context to TriggerContext

--- src/handlers/scheduler.ts
+++ src/handlers/scheduler.ts
@@ -1,5 +1,5 @@
-import {Context, ScheduledJobEvent} from "@devvit/public-api";
+import {TriggerContext, ScheduledJobEvent} from "@devvit/public-api";
 
-export async function someRecurringTask (event: ScheduledJobEvent, context: Context) {
+export async function someRecurringTask (event: ScheduledJobEvent, context: TriggerContext) {
     console.log(`someRecurringTask job ran at ${new Date().toISOString()}\nevent:\n${JSON.stringify(event)}\ncontext:\n${JSON.stringify(context)}`);
 }
